a3f2fecf94087cf8ba6e9a0adc786bd37acf0c63
corvo <corvolino@protonmail.com>
2021-07-25 03:34:44 -0300

    update

--- atividade12.py
+++ atividade12.py
@@ -3,4 +3,4 @@ Tendo como dados de entrada a altura de uma pessoa,
 construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 '''
 
-teste
+testeaaaaaaaaaaaaaaaaaa
