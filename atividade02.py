ed893e9b93e5ca53c29d5a407463d04293e05629
corvo <corvolino@protonmail.com>
2021-07-25 16:08:30 -0300

    organização da pasta

--- atividade02.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-numero = float(input("Por favor digite um número: "))
-
-print("\nNúmero informado foi o {}".format(numero))
