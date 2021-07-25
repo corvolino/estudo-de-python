d4eea3c81e52261d8c96b7d08f9d6148d7d718a5
corvo <corvolino@protonmail.com>
2021-07-25 03:53:50 -0300

    resposta atividade 13

--- atividade13.py
+++ atividade13.py
@@ -5,3 +5,11 @@ utilizando as seguintes fórmulas:
     Para homens: (72.7*h) - 58
     Para mulheres: (62.1*h) - 44.7
 '''
+
+altura = float(input("\nInforme sua altura: "))
+
+alturahomem = (72.7 * altura) - 58
+alturamulher = (62.1 * altura) - 44.7
+
+print("\nPeso ideal para o homem é {:.2f} kilos".format(alturahomem))
+print("Peso ideal para a mulher é {:.2f} kilos".format(alturamulher))
