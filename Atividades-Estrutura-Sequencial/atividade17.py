2c37e5911d8f63048030998360680a13759c3413
corvo <corvolino@protonmail.com>
2021-07-25 21:49:54 -0300

    resposta questão 17

--- Atividades-Estrutura-Sequencial/atividade17.py
+++ Atividades-Estrutura-Sequencial/atividade17.py
@@ -9,3 +9,27 @@ a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metr
     misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e
     sempre arredonde os valores para cima, isto é, considere latas cheias.
 '''
+
+valordalata = 80.00
+valordogalao = 25.00
+
+metros = int(input("\nQual a quantidade de metros a ser pintada: "))
+
+qtdlitroslata = (metros / 6) + (10/100)
+qtdlata = qtdlitroslata / 18
+preco18lata = round(qtdlata + 0.5) * valordalata
+###################################################
+qtdlitrosgalao = (metros / 6) + (10/100)
+qtdgaloes = round((qtdlitrosgalao + 0.5) / 3.6)
+precolatoes = round(qtdgaloes) * valordogalao
+
+
+print("\nQuantidade de tinta = {:.2f} litros".format(float(qtdlitroslata)))
+print("Valor pago por Latas de 18 litros = {:.2f} reais".format(preco18lata))
+print("Quantidade de Latas necessário de 18 litros para pintar = {} lata(s)".format(int(round(qtdlata + 0.5))))
+#################
+print("\nQuantidade de Litros em Galões de 3.6 Litros = {:.2f} Litros".format(float(qtdlitrosgalao)))
+print("Quantidade de Galões de 3.6 Litros para pintar = {} Galões".format(int((qtdgaloes))))
+print("Valor pago por Galões de 3.6 litros = {:.2f} reais".format(precolatoes))
+###############
+print("\nQuantidade de Latas = {} Latas. Quantidade de Galões. = {} Galões".format(int(qtdlata), int(qtdgaloes)))
